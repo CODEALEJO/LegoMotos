f3235dff2319b46a4e92c7837576b88b627630cb
CODEALEJO <salomee579@gmail.com>
2025-06-03 10:52:15 -0500

    Tu mensaje de commit

--- obj/Debug/net8.0/LavaderoMotos.AssemblyInfo.cs
+++ obj/Debug/net8.0/LavaderoMotos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LavaderoMotos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eab30539f7fe14dd315d21cbda47a10cbbdc92a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933aad372c7d39d755d0f4148603e99ab9d3520c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LavaderoMotos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LavaderoMotos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
